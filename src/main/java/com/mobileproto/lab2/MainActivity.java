286d004cdb1f336d3a9307a4d1fc7225eb1be6ea
EvanSimpson <esimpson.rescue@gmail.com>
2013-09-15 14:12:28 -0700

    Takes title and note text, saves to file and lists all files

--- src/main/java/com/mobileproto/lab2/MainActivity.java
+++ src/main/java/com/mobileproto/lab2/MainActivity.java
@@ -1,8 +1,21 @@
 package com.mobileproto.lab2;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +23,45 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView title = (TextView) findViewById(R.id.titleField);
+
+
+        final TextView note = (TextView) findViewById(R.id.noteField);
+
+
+        List<String> files = new ArrayList<String>(Arrays.asList(fileList()));
+
+        final ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, files);
+
+        final ListView notes = (ListView) findViewById(R.id.noteList);
+
+        notes.setAdapter(aa);
+
+        Button save = (Button)findViewById(R.id.saveButton);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fileName = title.getText().toString();
+                String noteText = note.getText().toString();
+                if (fileName != null && noteText != null){
+                    try{
+                        FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+                        fos.write(noteText.getBytes());
+                        fos.close();
+                        title.setText("");
+                        note.setText("");
+                        aa.insert(fileName, 0);
+                        aa.notifyDataSetChanged();
+                    }catch (IOException e){
+                        Log.e("IOException", e.getMessage());
+                    }
+                }
+            }
+        });
+
+
     }
 
 
---
+++ src/main/java/com/mobileproto/lab2/NoteListAdapter.java
@@ -0,0 +1,15 @@
+package com.mobileproto.lab2;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by evan on 9/15/13.
+ */
+public class NoteListAdapter extends ArrayAdapter {
+
+    @Override
+    public View getView(){
+
+    }
+}
